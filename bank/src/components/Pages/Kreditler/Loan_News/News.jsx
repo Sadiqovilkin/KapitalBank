a82eb7b8d02c2b8a25c60d869df38ca722aa2fa1
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-05-14 20:09:56 +0400

    #Loan page is done

--- bank/src/components/Pages/Kreditler/Loan_News/News.jsx
+++ bank/src/components/Pages/Kreditler/Loan_News/News.jsx
@@ -1,7 +1,100 @@
 import React from "react";
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <section id="loannews">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="news_head">
+              <p>
+                Bankdan kredit almaq, arzularınızı həyata keçirmək - bir klik
+                qədər asandır. Sadəcə şəxsiyyət vəsiqəsinin FİN kodunu daxil
+                etməklə kredit pul almaq mümkündür. Onlayn müraciət edərək
+                ehtiyaclarınıza uyğun krediti serfeli kredit şərtləri ilə vaxt
+                itirmədən əldə edin. Bütün şəxsi ehtiyaclarınız üçün Kapital
+                Bank yanınızda! Kreditlər, sürətli pul ehtiyacınızı qarşılamaq
+                üçün ən optimal seçimdir. İstifadə məqsədlərinə bağlı olaraq
+                ayrılan mikro biznes krediti kiçik sahibkarların biznesinin
+                inkişafına kömək edərkən, gündəlik tələbat, ipoteka krediti
+                fiziki şəxslərin ehtiyaclarını vaxtında qarşılamağa imkan
+                yaradır. Bank krediti banka girov qoyulma tələbinin
+                olub-olmamasına görə təminatlı və təminatsız olmaqla iki çeşiddə
+                təqdim olunur. Təminatlı bank kreditləri sizə daha aşağı kredit
+                faizləri təqdim edərkən, təminatsız kreditlər daha az risklə
+                ehtiyaclarınızı qarşılamağa kömək edir.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="tags">
+        <div className="container">
+          <ul>
+            <li>
+              <a href="#">kredit pul goturmek 2021</a>
+            </li>
+            <li>
+              <a href="#">bank kredit</a>
+            </li>
+            <li>
+              <a href="#">nağd kredit</a>
+            </li>
+            <li>
+              <a href="#">en serfeli kredit</a>
+            </li>
+            <li>
+              <a href="#">tek sexsiyyet vesiqesi ile kredit pul 2021</a>
+            </li>
+            <li>
+              <a href="#">ehtiyac krediti</a>
+            </li>
+            <li>
+              <a href="#">kredit kalkulyatoru</a>
+            </li>
+            <li>
+              <a href="#">kredit pul goturmek 2021</a>
+            </li>
+            <li>
+              <a href="#">kredit pul goturmek 2021</a>
+            </li>
+            <li>
+              <a href="#">nağd kredit</a>
+            </li>
+            <li>
+              <a href="#">en serfeli kredit</a>
+            </li>
+            <li>
+              <a href="#">kredit pul goturmek 2021</a>
+            </li>
+            <li>
+              <a href="#">kredit pul goturmek 2021</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="section_footer">
+        <div className="container">
+          <ul>
+            <li>
+              <a href="#" className="active_link">
+                Nağd krediti onlayn almaq 5% daha sərfəlidir!
+              </a>
+            </li>
+            <li>
+              <a href="#">Gündəlik tələbat krediti</a>
+            </li>
+            <li>
+              <a href="#">Mikro Biznes Kreditləri</a>
+            </li>
+            <li>
+              <a href="#">Təminatlı manat krediti</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default News;
